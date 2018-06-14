3c22251dd8993ea5915c9698f070f4a85de15ca5
Stefan Berger <stefanb@linux.vnet.ibm.com>
2018-06-14 17:13:11 -0400

    swtpm: Implement functions for supporting tag-length-value
    Implement functions for supporting tag-length-value headers
    in the byte stream we store the TPM's data into.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

---
+++ src/swtpm/tlv.c
@@ -0,0 +1,146 @@
+/*
+ * tlc.v -- tag-length-value
+ *
+ * (c) Copyright IBM Corporation 2018.
+ *
+ * Author: Stefan Berger <stefanb@us.ibm.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the names of the IBM Corporation nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+
+#include "tlv.h"
+
+#include <string.h>
+
+#include <libtpms/tpm_library.h>
+#include <libtpms/tpm_memory.h>
+
+void
+tlv_data_free(tlv_data *td, size_t td_len)
+{
+    size_t i;
+
+    for (i = 0; i < td_len; i++) {
+        if (!td[i].is_const_ptr)
+            TPM_Free(td[i].u.ptr);
+        memset(&td[i], 0, sizeof(*td));
+    }
+}
+
+/*
+ * tlv_data_append: append data in tlv_data array to a buffer
+ * @buffer: pointer to a pointer to a buffer or NULL if new buffer
+ * @buffer_len: length of existing buffer; will hold size of new buffer on return
+ * @td: array of tlv_data
+ * @td_len: length of td array
+ */
+TPM_RESULT
+tlv_data_append(unsigned char **buffer, uint32_t *buffer_len,
+                tlv_data *td, size_t td_len)
+{
+    TPM_RESULT res;
+    size_t i;
+    tlv_header tlv;
+    uint32_t totlen;
+    size_t addlen = 0;
+    unsigned char *ptr;
+
+    for (i = 0; i < td_len; i++)
+        addlen += sizeof(tlv) + td[i].tlv.length;
+
+    if (*buffer)
+        totlen = *buffer_len + addlen;
+    else
+        totlen = addlen;
+
+    res = TPM_Realloc(buffer, totlen);
+    if (res)
+         return res;
+
+    ptr = *buffer + *buffer_len;
+    *buffer_len = totlen;
+
+    for (i = 0; i < td_len; i++) {
+        tlv.tag = htobe16(td[i].tlv.tag);
+        tlv.length = htobe32(td[i].tlv.length);
+
+        memcpy(ptr, &tlv, sizeof(tlv));
+        ptr += sizeof(tlv);
+
+        if (td[i].is_const_ptr)
+            memcpy(ptr, td[i].u.const_ptr, td[i].tlv.length);
+        else
+            memcpy(ptr, td[i].u.ptr, td[i].tlv.length);
+        ptr += td[i].tlv.length;
+    }
+
+    return 0;
+}
+
+/* tlv_data_find_tag: in a byte stream that starts with a tlv_header,
+                      find a tlv_header with a given tag
+ * @buffer: the buffer to search; must start with a tlv_header
+ * @buffer_len: the length of the buffer
+ * @tag: the tag to search for
+ * @td: tlv_data pointer to receive the result in,
+ *
+ * Returns NULL if nothing was found, the pointer to the data corresponding
+ * to the tag otherwise.
+ */
+const unsigned char *
+tlv_data_find_tag(const unsigned char *buffer, uint32_t buffer_len,
+                  uint16_t tag, tlv_data *td)
+{
+    uint32_t offset = 0;
+
+    while (offset < buffer_len) {
+        if (offset + sizeof(td->tlv) > buffer_len)
+            return NULL;
+
+        memcpy(&td->tlv, buffer + offset, sizeof(td->tlv));
+        offset += sizeof(td->tlv);
+
+        td->tlv.length = be32toh(td->tlv.length);
+        if (offset + td->tlv.length > buffer_len)
+            return NULL;
+
+        td->tlv.tag = be16toh(td->tlv.tag);
+        if (td->tlv.tag == tag) {
+            td->is_const_ptr = true;
+            td->u.const_ptr = &buffer[offset];
+            return buffer;
+        }
+        offset += td->tlv.length;
+    }
+    return NULL;
+}
+
---
+++ src/swtpm/tlv.h
@@ -0,0 +1,90 @@
+/*
+ * tlv.h -- tag-length-value
+ *
+ * (c) Copyright IBM Corporation 2018.
+ *
+ * Author: Stefan Berger <stefanb@us.ibm.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the names of the IBM Corporation nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _SWTPM_TLV_H_
+#define _SWTPM_TLV_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include <libtpms/tpm_types.h>
+
+typedef struct tlv_header {
+    uint16_t tag;
+    uint32_t length; /* length of the data to follow excluding this header */
+    /* uint8_t data[0]; */
+} __attribute((packed)) tlv_header;
+
+#define TAG_DATA                     1
+#define TAG_ENCRYPTED_DATA           2
+#define TAG_HMAC                     3
+#define TAG_MIGRATION_DATA           4
+#define TAG_ENCRYPTED_MIGRATION_DATA 5
+
+typedef struct tlv_data {
+    struct tlv_header tlv;
+    bool is_const_ptr;
+    union {
+        uint8_t *ptr;
+        const uint8_t *const_ptr;
+    } u;
+} tlv_data;
+
+#define _TLV_DATA(TAG, LENGTH, IS_CONST_PTR, PTR_FIELD) \
+    (tlv_data) { \
+        .tlv.tag = TAG, \
+        .tlv.length = LENGTH, \
+        .is_const_ptr = IS_CONST_PTR, \
+        PTR_FIELD, \
+    }
+
+#define TLV_DATA(TAG, LENGTH, PTR)       _TLV_DATA(TAG, LENGTH, false,\
+                                                   .u.ptr = PTR)
+#define TLV_DATA_CONST(TAG, LENGTH, PTR) _TLV_DATA(TAG, LENGTH, true ,\
+                                                   .u.const_ptr = PTR)
+
+void tlv_data_free(tlv_data *td, size_t td_len);
+
+TPM_RESULT tlv_data_append(unsigned char **buffer, uint32_t *buffer_len,
+                           tlv_data *td, size_t td_len);
+
+const unsigned char *tlv_data_find_tag(const unsigned char *buffer,
+                                       uint32_t buffer_len,
+                                       uint16_t tag, tlv_data *td);
+
+#endif /* _SWTPM_TLV_H_ */
