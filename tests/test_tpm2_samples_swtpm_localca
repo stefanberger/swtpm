stefanberger/swtpm

commit 961bb827a42f59a69ab056167787950e510d7bb8

samples: Use new pwd format option for secretly passing passwords
Use the swtpm_cert --signkey-pwd and --parentkey-pwd to pass key passwords
using files rather than using the command line options.

Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

samples/swtpm-localca.in
@@ -203,14 +203,6 @@ create_cert() {
 		options=$(cat "${LOCALCA_OPTIONS}")
 	fi
 
-	if [ -n "${SIGNKEY_PASSWORD}" ]; then
-		options="$options --signkey-password \"${SIGNKEY_PASSWORD}\""
-	fi
-
-	if [ -n "${PARENTKEY_PASSWORD}" ]; then
-		options="$options --parentkey-password ${PARENTKEY_PASSWORD}"
-	fi
-
 	if [ -n "$vmid" ]; then
 		options="$options --subject \"CN=$vmid\""
 	else
@@ -258,6 +250,8 @@ create_cert() {
 		else
 			eval swtpm_cert \
 			$options \
+			${SIGNKEY_PASSWORD:+--signkey-pwd file:<(echo -en "$SIGNKEY_PASSWORD")} \
+			${PARENTKEY_PASSWORD:+--parentkey-pwd file:<(echo -en "$PARENTKEY_PASSWORD")} \
 			$tpm_spec_params \
 			$tpm_attr_params \
 			${skpkcs:+--signkey "$skpkcs"} \

commit 403dc65e4bf5b984f8d9aca7b7f33f865b1e036f

samples: Apply password for signing key when creating platform cert (bugfix)
Apply the password for the signing key and parent key when creating a
platform certificate. The bug was introduced in commit 961bb827
"samples: Use new pwd format option for secretly passing passwords"

Extend a test case to ensure that the passwords are always used.

Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

samples/swtpm-localca.in
@@ -290,6 +290,8 @@ create_cert() {
 			swtpm_cert \
 			--subject "$subj" \
 			$options \
+			${SIGNKEY_PASSWORD:+--signkey-pwd file:<(echo -en "$SIGNKEY_PASSWORD")} \
+			${PARENTKEY_PASSWORD:+--parentkey-pwd file:<(echo -en "$PARENTKEY_PASSWORD")} \
 			$tpm_attr_params \
 			--type platform \
 			--signkey "${SIGNKEY}" \

tests/test_tpm2_samples_swtpm_localca
@@ -41,6 +41,7 @@ statedir=${workdir}
 signingkey = ${SIGNINGKEY}
 issuercert = ${ISSUERCERT}
 certserial = ${CERTSERIAL}
+signingkey_password = password
 _EOF_
 
 cat <<_EOF_ > "${workdir}/swtpm-localca.options"
@@ -76,6 +77,27 @@ do
   if [ $? -ne 0 ]; then
     echo "Error: Test with parameters '$params' failed."
     exit 1
+  fi
+
+  # Signing key should always be password protected
+  if [ -z "$(grep "ENCRYPTED PRIVATE KEY" "${SIGNINGKEY}")" ]; then
+    echo "Error: Signing key is not password protected."
+    exit 1
+  fi
+
+  # For the root CA's key we flip the password protection
+  if [ -n "${SWTPM_ROOTCA_PASSWORD}" ] ;then
+     if [ -z "$(grep "ENCRYPTED PRIVATE KEY" "${workdir}/swtpm-localca-rootca-privkey.pem")" ]; then
+       echo "Error: Root CA's private key is not password protected."
+       exit 1
+     fi
+     unset SWTPM_ROOTCA_PASSWORD
+  else
+     if [ -n "$(grep "ENCRYPTED PRIVATE KEY" "${workdir}/swtpm-localca-rootca-privkey.pem")" ]; then
+       echo "Error: Root CA's private key is password protected but should not be."
+       exit 1
+     fi
+     export SWTPM_ROOTCA_PASSWORD=xyz
   fi
 
   if [ ! -r "${workdir}/ek.cert" ]; then
@@ -114,6 +136,9 @@ do
     echo "Error: Could not verify certificate chain."
     exit 1
   fi
+
+  # Delete all keys to have CA re-created
+  rm -rf "${workdir}"/*.pem
 done
 
 echo "Test 1: OK"