c125e34bf23afa8e0f0b876c352310b7b46ae826
Stefan Berger <stefanb@linux.ibm.com>
2021-05-17 07:36:36 -0400

    swtpm_setup: Rewrite in 'C' (addresses issue #437)
    This is a 1:1 rewrite of swtpm_setup in 'C' as a first step to get
    rid of the python dependency. It addresses issue #437.
    
    Tested on: Cygwin, Fedora 33, Ubuntu Xenial & Bionic, FreeBSD,
               DragonFlyBSD, OpenBSD (i386), Mac, Debian (recent), Centos-8,
               CentOS (recent), Alpine (recent), OpenSUSE (recent)
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ src/swtpm_setup/swtpm.h
@@ -0,0 +1,98 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * swtpm.h: Header file for swtpm.c
+ *
+ * Author: Stefan Berger, stefanb@linux.ibm.com
+ *
+ * Copyright (c) IBM Corporation, 2021
+ */
+
+#ifndef SWTPM_SETUP_SWTPM_H
+#define SWTPM_SETUP_SWTPM_H
+
+#include <glib.h>
+
+#include <openssl/sha.h>
+
+struct swtpm;
+
+/* common swtpm ops for TPM 1.2 & TPM 2 */
+struct swtpm_cops {
+    int (*start)(struct swtpm *);
+    void (*stop)(struct swtpm *);
+    void (*destroy)(struct swtpm *);
+    int (*remove_pidfile)(struct swtpm *);
+
+    int (*ctrl_shutdown)(struct swtpm *);
+    int (*ctrl_get_tpm_specs_and_attrs)(struct swtpm *, gchar **);
+};
+
+/* TPM 1.2 specific ops */
+struct swtpm12_ops {
+    int (*run_swtpm_bios)(struct swtpm *self);
+    int (*create_endorsement_key_pair)(struct swtpm *, gchar **pubkey, size_t *pubek_len);
+    int (*take_ownership)(struct swtpm *self,
+                          const unsigned char ownerpass_digest[SHA_DIGEST_LENGTH],
+                          const unsigned char srkpass_digest[SHA_DIGEST_LENGTH],
+                          const unsigned char *pubek, size_t pubek_len);
+    int (*write_ek_cert_nvram)(struct swtpm *self,
+                               const unsigned char *data, size_t data_len);
+    int (*write_platform_cert_nvram)(struct swtpm *self,
+                                     const unsigned char *data, size_t data_len);
+    int (*nv_lock)(struct swtpm *self);
+};
+
+/* TPM 2 specific ops */
+struct swtpm2_ops {
+    int (*shutdown)(struct swtpm *);
+    int (*create_spk)(struct swtpm *self, gboolean isecc, unsigned int rsa_keysize);
+    int (*create_ek)(struct swtpm *self, gboolean isecc, unsigned int rsa_keysize,
+                     gboolean allowsigning, gboolean decryption, gboolean lock_nvram,
+                     gchar **ekparam);
+    int (*get_all_pcr_banks)(struct swtpm *self, gchar ***all_pcr_banks);
+    int (*set_active_pcr_banks)(struct swtpm *self, gchar **pcr_banks_l, gchar **all_pcr_banks,
+                                gchar ***active);
+    int (*write_ek_cert_nvram)(struct swtpm *self, gboolean isecc, unsigned int rsa_keysize,
+                               gboolean lock_nvram, const unsigned char *data, size_t data_len);
+    int (*write_platform_cert_nvram)(struct swtpm *self, gboolean lock_nvram,
+                                     const unsigned char *data, size_t data_len);
+};
+
+/* common structure for swtpm object */
+struct swtpm {
+    const struct swtpm_cops *cops;
+    gchar **swtpm_exec_l;
+    const gchar *state_path;
+    const gchar *keyopts;
+    const gchar *logfile;
+    const int *fds_to_pass;
+    size_t n_fds_to_pass;
+    gboolean is_tpm2;
+
+    GPid pid;
+    gchar *pidfile;
+    int ctrl_fds[2];
+    int data_fds[2];
+};
+
+struct swtpm12 {
+    struct swtpm swtpm;
+    const struct swtpm12_ops *ops;
+};
+
+struct swtpm2 {
+    struct swtpm swtpm;
+    const struct swtpm2_ops *ops;
+};
+
+struct swtpm12 *swtpm12_new(gchar **swtpm_prg_l, const gchar *tpm_state_path,
+                            const gchar *swtpm_keyopts, const gchar *logfile,
+                            int *fds_to_pass, size_t n_fds_to_pass);
+
+struct swtpm2 *swtpm2_new(gchar **swtpm_prg_l, const gchar *tpm_state_path,
+                         const gchar *swtpm_keyopts, const gchar *logfile,
+                         int *fds_to_pass, size_t n_fds_to_pass);
+
+void swtpm_free(struct swtpm *);
+
+#endif /* SWTPM_SETUP_SWTPM_H */
---
+++ src/swtpm_setup/swtpm_setup_utils.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * swtpm_setup_utils.h: Header for swtpm_setup_utils.c
+ *
+ * Author: Stefan Berger, stefanb@linux.ibm.com
+ *
+ * Copyright (c) IBM Corporation, 2021
+ */
+
+#ifndef SWTPM_SETUP_UTILS_H
+#define SWTPM_SETUP_UTILS_H
+
+#include <glib.h>
+
+gchar *get_config_value(gchar **config_file_lines, const gchar *configname);
+
+#endif /* SWPTM_SETUP_UTILS_H */
