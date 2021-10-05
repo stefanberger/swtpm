4a5acef4d78a26c379670f10c4e7d0102ee440bb
Stefan Berger <stefanb@linux.ibm.com>
2021-10-05 11:37:54 -0400

    headers: Add defines for little endian data conversion (OS X)
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- include/sys_dependencies.h
+++ include/sys_dependencies.h
@@ -58,6 +58,14 @@
 # define htobe32(x) OSSwapHostToBigInt32(x)
 # define htobe64(x) OSSwapHostToBigInt64(x)
 
+# define le16toh(x) OSSwapLittleToHostInt16(x)
+# define le32toh(x) OSSwapLittleToHostInt32(x)
+# define le64toh(x) OSSwapLittleToHostInt64(x)
+
+# define htole16(x) OSSwapHostToLittleInt16(x)
+# define htole32(x) OSSwapHostToLittleInt32(x)
+# define htole64(x) OSSwapHostToLittleInt64(x)
+
 #else
 # include <endian.h>
 #endif
