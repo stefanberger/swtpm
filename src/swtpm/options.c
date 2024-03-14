stefanberger/swtpm

commit c94a1c4cbdae46f98f1d35b01195c45e71ea1724

swtpm: Fix memory leak in case realloc fails
Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

src/swtpm/options.c
@@ -84,14 +84,16 @@ option_value_add(OptionValues *ovs, const OptionDesc optdesc, const char *val,
     long unsigned int lui;
     struct passwd *passwd;
     struct group *group;
-    
     size_t idx = ovs->n_options;
-    
-    ovs->options = realloc(ovs->options, (idx + 1) * sizeof(*ovs->options));
-    if (!ovs->options) {
+    void *tmp;
+
+    tmp = realloc(ovs->options, (idx + 1) * sizeof(*ovs->options));
+    if (!tmp) {
+        free(ovs->options);
         option_error_set(error, "Out of memory");
         return -1;
     }
+    ovs->options = tmp;
 
     ovs->n_options = idx + 1;
     ovs->options[idx].type = optdesc.type;

commit af25baf39471ea235448ea8474f69cccbed65550

swtpm: Fix double free in error path
On error, the caller will call options_value_free() so don't free
ovs->options to avoid a double free.

Fixes: c94a1c4cbdae ("swtpm: Fix memory leak in case realloc fails")
Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

src/swtpm/options.c
@@ -89,7 +89,6 @@ option_value_add(OptionValues *ovs, const OptionDesc optdesc, const char *val,
 
     tmp = realloc(ovs->options, (idx + 1) * sizeof(*ovs->options));
     if (!tmp) {
-        free(ovs->options);
         option_error_set(error, "Out of memory");
         return -1;
     }