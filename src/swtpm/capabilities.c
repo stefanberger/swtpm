stefanberger/swtpm

commit 96fe5afac7c39d7a0e2074f00d179878d859bc84

swtpm: Add support for --print-profiles option
Add support for --print-profiles option to print all profiles supported
by libtpms.

Usage:

  swtpm socket --tpm2 --print-profiles | jq

Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

man/man8/swtpm.pod
@@ -334,7 +334,8 @@ may contain the following:
         "rsa-keysize-1024",
         "rsa-keysize-2048",
         "rsa-keysize-3072",
-        "cmdarg-profile"
+        "cmdarg-profile",
+        "cmdarg-print-profile"
       ],
       "version": "0.7.0"
     }
@@ -414,6 +415,10 @@ rsa-keysize verbs is shown then only RSA 2048 bit keys are supported.
 The option <--profile> is supported to set a profile for a TPM 2 using either
 the option parameter I<name=> to select a profile by its name or I<profile=>
 to provide a JSON-formatted profile.
+
+=item B<cmdarg-print-profiles> (since v0.10)
+
+The option <--print-profiles> is supported.
 
 =back
 
@@ -541,6 +546,10 @@ I<swtpm_ioctl> tool:
         "Description": "This profile enables all currenly supported commands and algorithms. It is applied when the user chooses no profile."
       }
     }
+
+=item B<--print-profiles> (since v0.10)
+
+Display the profiles supported by libtpms. Use with I<--tpm2> option.
 
 =item B<-h|--help>
 

src/swtpm/capabilities.c
@@ -218,6 +218,18 @@ error:
     return ret;
 }
 
+int print_profiles(void)
+{
+    char *info_data = TPMLIB_GetInfo(TPMLIB_INFO_AVAILABLE_PROFILES);
+
+    if (info_data)
+        printf("%s", info_data);
+
+    free(info_data);
+
+    return 0;
+}
+
 int capabilities_print_json(bool cusetpm, TPMLIB_TPMVersion tpmversion)
 {
     char *string = NULL;

src/swtpm/capabilities.h
@@ -44,4 +44,6 @@
 
 int capabilities_print_json(bool cusetpm, TPMLIB_TPMVersion tpmversion);
 
+int print_profiles(void);
+
 #endif /* SWTPM_CAPABILITIES_H */

src/swtpm/cuse_tpm.c
@@ -279,6 +279,8 @@ static const char *usage =
 "--print-states      : print existing TPM states and terminate\n"
 "--profile name=<name>|profile=<json-profile>\n"
 "                    : Set a profile on the TPM 2\n"
+"--print-profiles\n"
+"                    : print all profiles supported by libtpms\n"
 "-h|--help           :  display this help screen and terminate\n"
 "\n";
 
@@ -1611,6 +1613,7 @@ int swtpm_cuse_main(int argc, char **argv, const char *prgname, const char *ifac
                          ,       no_argument, 0, 'a'},
         {"print-states"  ,       no_argument, 0, 'e'},
         {"profile"       , required_argument, 0, 'I'},
+        {"print-profiles",       no_argument, 0, 'N'},
         {NULL            , 0                , 0, 0  },
     };
     struct cuse_info cinfo;
@@ -1627,6 +1630,7 @@ int swtpm_cuse_main(int argc, char **argv, const char *prgname, const char *ifac
     int ret = 0;
     bool printcapabilities = false;
     bool printstates = false;
+    bool printprofiles = false;
     bool need_init_cmd = true;
     TPM_RESULT res;
 
@@ -1742,6 +1746,9 @@ int swtpm_cuse_main(int argc, char **argv, const char *prgname, const char *ifac
         case 'e':
             printstates = true;
             break;
+        case 'N': /* --print-profiles */
+            printprofiles = true;
+            break;
         case 'v': /* version */
             fprintf(stdout, "TPM emulator CUSE interface version %d.%d.%d, "
                     "Copyright (c) 2014-2015 IBM Corp.\n",
@@ -1816,6 +1823,11 @@ int swtpm_cuse_main(int argc, char **argv, const char *prgname, const char *ifac
         }
         ret = SWTPM_NVRAM_PrintJson();
         ret = ret ? EXIT_FAILURE : EXIT_SUCCESS;
+        goto exit;
+    }
+
+    if (printprofiles) {
+        print_profiles();
         goto exit;
     }
 

src/swtpm/swtpm.c
@@ -199,6 +199,8 @@ static void usage(FILE *file, const char *prgname, const char *iface)
     "                 : print existing TPM states and terminate\n"
     "--profile name=<name>|profile=<json-profile>\n"
     "                 : Set a profile on the TPM 2\n"
+    "--print-profiles\n"
+    "                 : print all profiles supported by libtpms\n"
     "-h|--help        : display this help screen and terminate\n"
     "\n",
     prgname, iface);
@@ -258,6 +260,7 @@ int swtpm_main(int argc, char **argv, const char *prgname, const char *iface)
     unsigned int seccomp_action;
     bool printcapabilities = false;
     bool printstates = false;
+    bool printprofiles = false;
     static struct option longopts[] = {
         {"daemon"    ,       no_argument, 0, 'd'},
         {"help"      ,       no_argument, 0, 'h'},
@@ -284,6 +287,7 @@ int swtpm_main(int argc, char **argv, const char *prgname, const char *iface)
                      ,       no_argument, 0, 'a'},
         {"print-states",     no_argument, 0, 'e'},
         {"profile"   , required_argument, 0, 'I'},
+        {"print-profiles",   no_argument, 0, 'N'},
         {NULL        , 0                , 0, 0  },
     };
 
@@ -434,6 +438,10 @@ int swtpm_main(int argc, char **argv, const char *prgname, const char *iface)
             profiledata = optarg;
             break;
 
+        case 'N': /* --print-profiles */
+            printprofiles = true;
+            break;
+
         default:
             usage(stderr, prgname, iface);
             exit(EXIT_FAILURE);
@@ -499,6 +507,11 @@ int swtpm_main(int argc, char **argv, const char *prgname, const char *iface)
             goto exit_success;
         else
             goto exit_failure;
+    }
+
+    if (printprofiles) {
+        print_profiles();
+        goto exit_success;
     }
 
     if (handle_key_options(keydata) < 0 ||

src/swtpm/swtpm_chardev.c
@@ -220,6 +220,8 @@ static void usage(FILE *file, const char *prgname, const char *iface)
     "                 : print existing TPM states and terminate\n"
     "--profile name=<name>|profile=<json-profile>\n"
     "                 : Set a profile on the TPM 2\n"
+    "--print-profiles\n"
+    "                 : print all profiles supported by libtpms\n"
     "-h|--help        : display this help screen and terminate\n"
     "\n",
     prgname, iface);
@@ -316,6 +318,7 @@ int swtpm_chardev_main(int argc, char **argv, const char *prgname, const char *i
     unsigned int seccomp_action;
     bool printcapabilities = false;
     bool printstates = false;
+    bool printprofiles = false;
     static struct option longopts[] = {
         {"daemon"    ,       no_argument, 0, 'd'},
         {"help"      ,       no_argument, 0, 'h'},
@@ -343,6 +346,7 @@ int swtpm_chardev_main(int argc, char **argv, const char *prgname, const char *i
                      ,       no_argument, 0, 'a'},
         {"print-states",     no_argument, 0, 'e'},
         {"profile"   , required_argument, 0, 'I'},
+        {"print-profiles",   no_argument, 0, 'N'},
         {NULL        , 0                , 0, 0  },
     };
 
@@ -484,6 +488,10 @@ int swtpm_chardev_main(int argc, char **argv, const char *prgname, const char *i
             profiledata = optarg;
             break;
 
+        case 'N': /* --print-profiles */
+            printprofiles = true;
+            break;
+
         default:
             usage(stderr, prgname, iface);
             exit(EXIT_FAILURE);
@@ -547,6 +555,11 @@ int swtpm_chardev_main(int argc, char **argv, const char *prgname, const char *i
             goto exit_failure;
         else
             goto exit_success;
+    }
+
+    if (printprofiles) {
+        print_profiles();
+        goto exit_success;
     }
 
     if (mlp.fd < 0) {

commit 587687dfe49b8ca34007638de10ba642590ceaf7

swtpm: Print cmdarg-print-profiles as part of capabilities
Commit 96fe5afa forgot to add cmdarg-print-profiles to the list of
capabilities. Also fix typo in the man page and sort shown output
to match application output.

Fixes: 96fe5afa ("swtpm: Add support for --print-profiles option")
Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

man/man8/swtpm.pod
@@ -320,6 +320,9 @@ may contain the following:
       "features": [
         "tpm-1.2",
         "tpm-2.0",
+        "tpm-send-command-header",
+        "flags-opt-startup",
+        "flags-opt-disable-auto-shutdown",
         "cmdarg-seccomp",
         "cmdarg-key-fd",
         "cmdarg-pwd-fd",
@@ -328,14 +331,11 @@ may contain the following:
         "cmdarg-migration",
         "nvram-backend-dir",
         "nvram-backend-file",
-        "tpm-send-command-header",
-        "flags-opt-startup",
-        "flags-opt-disable-auto-shutdown",
         "rsa-keysize-1024",
         "rsa-keysize-2048",
         "rsa-keysize-3072",
         "cmdarg-profile",
-        "cmdarg-print-profile"
+        "cmdarg-print-profiles"
       ],
       "version": "0.7.0"
     }

src/swtpm/capabilities.c
@@ -269,7 +269,7 @@ int capabilities_print_json(bool cusetpm, TPMLIB_TPMVersion tpmversion)
          "{ "
          "\"type\": \"swtpm\", "
          "\"features\": [ "
-             "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s"
+             "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s"
           " ], "
          "\"profiles\": { %s}, "
          "\"version\": \"" VERSION "\" "
@@ -290,6 +290,7 @@ int capabilities_print_json(bool cusetpm, TPMLIB_TPMVersion tpmversion)
          nvram_backend_file,
          keysizecaps  ? keysizecaps                    : "",
          true         ? ", \"cmdarg-profile\""         : "",
+         true         ? ", \"cmdarg-print-profiles\""  : "",
          profiles     ? profiles                       : ""
     );
 

tests/_test_print_capabilities
@@ -28,7 +28,8 @@ exp='\{ "type": "swtpm", '\
 '"features": \[ "tpm-1.2",( "tpm-2.0",)? '${noncuse}'"flags-opt-startup", '\
 '"flags-opt-disable-auto-shutdown", "ctrl-opt-terminate", '${seccomp}'"cmdarg-key-fd", '\
 '"cmdarg-pwd-fd", "cmdarg-print-states", "cmdarg-chroot", "cmdarg-migration", '\
-'"nvram-backend-dir", "nvram-backend-file"(, "cmdarg-profile")? \],'\
+'"nvram-backend-dir", "nvram-backend-file", "cmdarg-profile", '\
+'"cmdarg-print-profiles" \],'\
 '( "profiles": \{ \},)? '\
 '"version": "[^"]*" \}'
 if ! [[ ${msg} =~ ${exp} ]]; then

tests/_test_tpm2_print_capabilities
@@ -30,7 +30,8 @@ exp='\{ "type": "swtpm", '\
 '"flags-opt-disable-auto-shutdown", "ctrl-opt-terminate", '${seccomp}'"cmdarg-key-fd", '\
 '"cmdarg-pwd-fd", "cmdarg-print-states", "cmdarg-chroot", "cmdarg-migration", '\
 '"nvram-backend-dir", "nvram-backend-file"'\
-'(, "rsa-keysize-1024")?(, "rsa-keysize-2048")?(, "rsa-keysize-3072")?(, "cmdarg-profile")? \],'\
+'(, "rsa-keysize-1024")?(, "rsa-keysize-2048")?(, "rsa-keysize-3072")?, "cmdarg-profile", '\
+'"cmdarg-print-profiles" \],'\
 '( "profiles": \{ "names": \[ [^]]*\], "algorithms": \{ [^\}]*\}, "commands": \{ [^\}]*\} },)? '\
 '"version": "[^"]*" \}'
 if ! [[ ${msg} =~ ${exp} ]]; then