3b775987c0ac5385df98127f640d1214e288bacb
Stefan Berger <stefanb@linux.ibm.com>
2026-08-17 18:07:28 -0400

    swtpm_setup: Check the SETUP_EK_CERT_F flag when creating IAK or IDevIDs
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/swtpm_setup/swtpm_setup.c
+++ src/swtpm_setup/swtpm_setup.c
@@ -2352,7 +2352,7 @@ int main(int argc, char *argv[])
             goto error;
 
         if ((iakkeyalgo != KEYALGO_NONE || idevidkeyalgo != KEYALGO_NONE) &&
-            (flags & SETUP_CREATE_EK_F)== 0 ) {
+            (flags & SETUP_EK_CERT_F) == 0 ) {
             fprintf(stderr, "When creating IAK and/or IDevID keys, then an EK certificate must be created as well.\n");
             goto error;
         }
